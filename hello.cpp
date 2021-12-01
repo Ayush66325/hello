63d22a35a2ceb018539908446208e8f545da5f68
Ayush66325 <d.dark.pheonix007@gmail.com>
2021-12-01 21:52:59 +0530

    Update hello.cpp

--- hello.cpp
+++ hello.cpp
@@ -1,1 +1,30 @@
-fghj
+
+#include<iostream>
+using namespace std;
+int main()
+{   int n,ARR[10],tar;
+    cout<<"Enter the limit of Array :\n";
+    cin>>n;
+    cout<<"Enter the Array :\n";
+    for(int i=0;i<n;i++)
+    {
+        cout<<"ARR["<<i<<"] :\n";
+        cin>>ARR[i];
+    }
+    cout<<"THE ARRAR IS :";
+    for(int j=o;j<n;j++)
+    {
+        cout<<ARR[i]<<endl;
+    }
+    cout<<"Enter Target :";
+    cin>>tar;
+
+//------------------------------------------------------
+    for(k=0;k<n;k++)
+    {
+        if(ARR[k]+ARR[k+1]==tar)
+        cout<<"["<<k<<" , "<<"] ----"<<tar;
+    }
+
+    return 0;
+}
